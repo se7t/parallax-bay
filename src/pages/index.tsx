c8c0fb8ce0afde512dfc4c95f89140ed5196f6de
se7t <se7t.contact@gmail.com>
2021-04-23 16:02:18 +0200

    feat: configure package.json, remove obsolete files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,6 @@
 import React from 'react';
 import { PageProps } from 'gatsby';
 
-import Title from '@/components/Title';
-
-const Home: React.FC<PageProps> = () => (
-  <main>
-    <Title />
-    <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-    <p>
-      Follow me on Twitter (
-      <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
-    </p>
-  </main>
-);
+const Home: React.FC<PageProps> = () => <p>Hi</p>;
 
 export default Home;
